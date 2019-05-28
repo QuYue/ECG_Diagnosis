4b16e599fc3bc5c4a142779d20713efd0985856c
Aisunny <1115468472@qq.com>
2019-05-28 14:01:48 +0800

    只修改了 参数

--- Workspace/Code/Experiment2_Multi_channel_CNN/models.py
+++ Workspace/Code/Experiment2_Multi_channel_CNN/models.py
@@ -19,14 +19,14 @@ class MC_CNN(nn.Module):
             nn.BatchNorm2d(num_features=32),
             nn.Dropout(0.8),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 1))
+            nn.MaxPool2d(kernel_size=(2, 1))
         )
         self.raw2 = nn.Sequential(
             nn.Conv2d(12, 32, kernel_size=(10, 1), stride=1, padding=(1, 0), bias=False),
             nn.BatchNorm2d(num_features=32),
             nn.Dropout(0.8),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 1))
+            nn.MaxPool2d(kernel_size=(2, 1))
         )
         self.maxpool = nn.MaxPool2d(kernel_size=(3, 1))
         # convolution
@@ -55,7 +55,7 @@ class MC_CNN(nn.Module):
             nn.MaxPool2d((3, 1))
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(64, 128, (5, 1), 1, 0),
+            nn.Conv2d(64, 128, (10, 1), 1, 0),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d((3, 1))
@@ -67,21 +67,16 @@ class MC_CNN(nn.Module):
             nn.MaxPool2d((3, 1))
         )
         self.conv6 = nn.Sequential(
-            nn.Conv2d(128, 128, (5, 1), 1, 0),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d((3, 1)))
-        self.conv7 = nn.Sequential(
             nn.Conv2d(128, 256, (3, 1), 1, 0),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d((3, 1))
         )
         self.network = nn.Sequential(
-            nn.Linear(512 * 1, 100),
+            nn.Linear(512 * 2, 100),
             nn.Dropout(0.5),
             nn.Linear(100, 60),
-            nn.Dropout(0.4),
+            nn.Dropout(0.5),
             nn.Linear(60, 9),
         )
 
@@ -93,12 +88,11 @@ class MC_CNN(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x= self.conv4(x)
-        x =self.conv5(x)
-        # x = torch.cat((x, x1), dim=2)
+        x1= self.conv4(x)
+        x =self.conv5(x1)
+        x = torch.cat((x, x1), dim=2)
         x =self.conv6(x)
-        x =self.conv7(x)
-        # x = nn.functional.avg_pool2d(x, 3)
+        x = nn.functional.avg_pool2d(x, 3)
         x = x.view(x.size(0), -1)
         output = self.network(x)
         return output
